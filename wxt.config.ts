7a46be79336285a5b9dde72c9bccd6a66f9cf540
u1aryz <u1aryz.d@gmail.com>
2025-07-15 17:36:43 +0900

    feat: setup biome and commitlint

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
-  outDir: "output",
+	modules: ["@wxt-dev/module-react"],
+	outDir: "output",
 });
