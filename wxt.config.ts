799554c8e885dfac0c06a6e1e167944746fde2b4
u1aryz <u1aryz.d@gmail.com>
2025-11-21 20:29:00 +0900

    feat: integrate webextension-polyfill module

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-	modules: ["@wxt-dev/module-react"],
+	modules: ["@wxt-dev/module-react", "@wxt-dev/webextension-polyfill"],
 	outDir: "output",
 	vite: () => ({
 		plugins: [tailwindcss()],
