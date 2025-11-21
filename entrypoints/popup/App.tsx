816ad71c9202f06331f0da318950a00187ea7e59
u1aryz <u1aryz.d@gmail.com>
2025-11-21 20:56:58 +0900

    feat: integrate daisyUI

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,7 +1,14 @@
 import { t } from "@/utils/i18n";
 
 function App() {
-	return <h1 className="text-2xl">{t("popupTitle")}</h1>;
+	return (
+		<div className="flex w-40 flex-col gap-2 p-4">
+			<h1 className="text-2xl">{t("popupTitle")}</h1>
+			<button type="button" className="btn btn-primary">
+				{t("popupButtonLabel")}
+			</button>
+		</div>
+	);
 }
 
 export default App;
